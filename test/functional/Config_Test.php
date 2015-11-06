1c882a3e740e1b165b748ac0b9ce0f698dd5a12a
Alex Gusev <alex@flancer64.com>
2015-11-06 16:07:05 +0200

    MOBI-107 - Create operation service in Accounting module

--- test/functional/Config_Test.php
+++ test/functional/Config_Test.php
@@ -9,7 +9,7 @@ include_once(__DIR__ . '/phpunit_bootstrap.php');
 class Config_UnitTest extends \PHPUnit_Framework_TestCase {
 
     public function test_lib() {
-        $ctx = \Praxigento\Downline\Lib\Context::get();
+        $ctx = \Praxigento\Downline\Lib\Context::instance();
         $this->assertTrue($ctx instanceof \Praxigento\Core\Lib\Context);
     }
 
