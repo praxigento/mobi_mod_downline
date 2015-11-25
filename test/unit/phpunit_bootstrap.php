b278c860313800533a5a10f6bdfac48d421f5ef8
Alex Gusev <alex@flancer64.com>
2015-11-25 13:35:47 +0200

    MOBI-127 - Improve configuration on deployment

--- test/unit/phpunit_bootstrap.php
+++ test/unit/phpunit_bootstrap.php
@@ -4,3 +4,6 @@
  * Path is relative to "./work/vendor/<vendor>/<module>/test/unit/" folder.
  */
 include_once(__DIR__ . '/../../../../autoload.php');
+if(!defined('IS_M2_TESTS')) {
+    define('IS_M2_TESTS', true);
+}
