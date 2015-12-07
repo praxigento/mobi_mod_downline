dd3476c526110eae7dae91a2cf7ab4ac9b175f4d
Alex Gusev <alex@flancer64.com>
2015-12-07 16:09:15 +0200

    MOBI-136 - Increase code coverage in core, accounting and downline modules

--- test/unit/Config_Test.php
+++ test/unit/Config_Test.php
@@ -6,7 +6,7 @@
 namespace Praxigento\Downline;
 include_once(__DIR__ . '/phpunit_bootstrap.php');
 
-class Config_UnitTest extends \PHPUnit_Framework_TestCase {
+class Config_UnitTest extends \Praxigento\Core\Lib\Test\BaseTestCase {
 
     public function test_constructor() {
         $cfg = new Config();
