4db88f1e5b27855da94933055ac8c8408b60c501
Alex Gusev <alex@flancer64.com>
2015-12-07 17:10:33 +0200

    MOBI-136 - Increase code coverage in core, accounting and downline modules

---
+++ test/unit/Setup/InstallSchema_Test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+namespace Praxigento\Downline\Setup;
+
+include_once(__DIR__ . '/../phpunit_bootstrap.php');
+
+class InstallSchema_UnitTest extends \Praxigento\Core\Lib\Test\BaseTestCase {
+
+    public function test_constructor() {
+        /** === Test Data === */
+        /** === Mocks === */
+        /** === Test itself === */
+        /** @var  $obj InstallSchema */
+        $obj = new InstallSchema();
+    }
+
+}
