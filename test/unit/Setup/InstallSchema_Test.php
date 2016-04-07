52f4135ecc2398627eea829616c3f4ff53febcb9
Alex Gusev <alex@flancer64.com>
2016-04-07 16:50:09 +0300

    MOBI-269 - Fix unit tests for InstallSchema

--- test/unit/Setup/InstallSchema_Test.php
+++ test/unit/Setup/InstallSchema_Test.php
@@ -4,16 +4,64 @@
  */
 namespace Praxigento\Downline\Setup;
 
+use Praxigento\Downline\Data\Entity\Change;
+use Praxigento\Downline\Data\Entity\Customer;
+use Praxigento\Downline\Data\Entity\Snap;
+
 include_once(__DIR__ . '/../phpunit_bootstrap.php');
 
-class InstallSchema_UnitTest extends \Praxigento\Core\Lib\Test\BaseMockeryCase {
+class InstallSchema_UnitTest extends \Praxigento\Core\Lib\Test\BaseMockeryCase
+{
 
-    public function test_constructor() {
-        /** === Test Data === */
-        /** === Mocks === */
-        /** === Test itself === */
-        /** @var  $obj InstallSchema */
-        $obj = new InstallSchema();
+    /** @var  \Mockery\MockInterface */
+    private $mConn;
+    /** @var  \Mockery\MockInterface */
+    private $mContext;
+    /** @var  \Mockery\MockInterface */
+    private $mSetup;
+    /** @var  \Mockery\MockInterface */
+    private $mToolDem;
+    /** @var  InstallSchema */
+    private $obj;
+
+    public function setUp()
+    {
+        parent::setUp();
+        /* create mocks */
+        $this->mConn = $this->_mockConn();
+        $this->mToolDem = $this->_mock(\Praxigento\Core\Setup\Dem\Tool::class);
+        $this->mSetup = $this->_mock(\Magento\Framework\Setup\SchemaSetupInterface::class);
+        $this->mContext = $this->_mock(\Magento\Framework\Setup\ModuleContextInterface::class);
+        /* create object */
+        $mResource = $this->_mockResourceConnection($this->mConn);
+        $this->obj = new InstallSchema($mResource, $this->mToolDem);
     }
 
+    public function test_install()
+    {
+        /* === Test Data === */
+        /* === Setup Mocks === */
+        // $setup->startSetup();
+        $this->mSetup
+            ->shouldReceive('startSetup')->once();
+        // $demPackage = $this->_toolDem->readDemPackage($pathToFile, $pathToNode);
+        $mDemPackage = $this->_mock(DataObject::class);
+        $this->mToolDem
+            ->shouldReceive('readDemPackage')->once()
+            ->withArgs([anything(), '/dBEAR/package/Praxigento/package/Downline'])
+            ->andReturn($mDemPackage);
+        // $demEntity = $demPackage->getData('package/Type/entity/Asset');
+        $mDemPackage->shouldReceive('getData');
+        //
+        // $this->_toolDem->createEntity($entityAlias, $demEntity);
+        //
+        $this->mToolDem->shouldReceive('createEntity')->withArgs([Customer::ENTITY_NAME, anything()]);
+        $this->mToolDem->shouldReceive('createEntity')->withArgs([Change::ENTITY_NAME, anything()]);
+        $this->mToolDem->shouldReceive('createEntity')->withArgs([Snap::ENTITY_NAME, anything()]);
+        // $setup->endSetup();
+        $this->mSetup
+            ->shouldReceive('endSetup')->once();
+        /* === Call and asserts  === */
+        $this->obj->install($this->mSetup, $this->mContext);
+    }
 }
