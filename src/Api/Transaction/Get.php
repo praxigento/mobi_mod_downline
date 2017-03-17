7be99ca05a4532e4fb8093f668fa8f633c83708a
Alex Gusev <alex@flancer64.com>
2017-03-17 15:37:46 +0200

    MOBI-658 - Unify DCP operations

--- src/Api/Transaction/Get.php
+++ src/Api/Transaction/Get.php
@@ -8,11 +8,26 @@ namespace Praxigento\Downline\Api\Transaction;
 class Get
     extends \Praxigento\Accounting\Api\Transaction\Get
 {
+    /** @var  \Praxigento\Downline\Repo\Query\Trans\Get\Builder */
+    protected $qbldDwnlTrans;
 
-    protected function getQueryBuilder()
-    {
-        $result = $this->manObj->get(\Praxigento\Downline\Repo\Query\Trans\Get\Builder::class);
-        return $result;
+    public function __construct(
+        \Praxigento\Core\Api\IAuthenticator $authenticator,
+        \Praxigento\Accounting\Repo\Query\Trans\Get\Builder $qbldTrans,
+        \Praxigento\Downline\Repo\Query\Trans\Get\Builder $qbldDwnlTrans
+    ) {
+        parent::__construct($authenticator, $qbldTrans);
+        $this->qbldDwnlTrans = $qbldDwnlTrans;
     }
 
+    /**
+     * Don't use original query builder. Replace with own one.
+     *
+     * @param \Flancer32\Lib\Data $ctx
+     */
+    protected function getSelectQuery(\Flancer32\Lib\Data $ctx)
+    {
+        $query = $this->qbldDwnlTrans->getSelectQuery();
+        $ctx->set(self::CTX_QUERY, $query);
+    }
 }
