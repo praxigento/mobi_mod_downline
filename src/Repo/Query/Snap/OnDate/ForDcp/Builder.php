897f19770492ad688c51737d51028f60a969599a
Alex Gusev <alex@flancer64.com>
2017-05-02 20:35:23 +0300

    MOBI-688 - Add Forecast calculation

--- src/Repo/Query/Snap/OnDate/ForDcp/Builder.php
+++ src/Repo/Query/Snap/OnDate/ForDcp/Builder.php
@@ -14,21 +14,25 @@ use Praxigento\Downline\Config as Cfg;
 class Builder
     extends \Praxigento\Core\Repo\Query\Def\Builder
 {
-    const AS_ATTR_COUNTRY_CODE = \Praxigento\Downline\Data\Entity\Customer::ATTR_COUNTRY_CODE;
-    const AS_ATTR_EMAIL = Cfg::E_CUSTOMER_A_EMAIL;
-    const AS_ATTR_MLM_ID = 'mlm_id';
-    const AS_ATTR_NAME_FIRST = 'name_first';
-    const AS_ATTR_NAME_LAST = 'name_last';
-    const AS_TBL_CUSTOMER = 'mageCust';
-    const AS_TBL_DOWNLINE_CUSTOMER = 'prxgtDwnlCust';
+    /** Tables aliases. */
+    const AS_CUSTOMER = 'mageCust';
+    const AS_DOWNLINE_CUSTOMER = 'prxgtDwnlCust';
+    /** Columns aliases. */
+    const A_COUNTRY_CODE = \Praxigento\Downline\Data\Entity\Customer::ATTR_COUNTRY_CODE;
+    const A_EMAIL = Cfg::E_CUSTOMER_A_EMAIL;
+    const A_MLM_ID = 'mlm_id';
+    const A_NAME_FIRST = 'name_first';
+    const A_NAME_LAST = 'name_last';
+    const A_NAME_MIDDLE = 'name_middle';
+    /** Bound variables names */
     const BIND_DATE = 'date';
 
     public function getSelectQuery(\Praxigento\Core\Repo\Query\IBuilder $qbuild = null)
     {
         $result = $qbuild->getSelectQuery();
-        $asDwnlCust = self::AS_TBL_DOWNLINE_CUSTOMER;
+        $asDwnlCust = self::AS_DOWNLINE_CUSTOMER;
         $asDwnlSnap = \Praxigento\Downline\Repo\Query\Snap\OnDate\Builder::AS_DWNL_SNAP;
-        $asCust = self::AS_TBL_CUSTOMER;
+        $asCust = self::AS_CUSTOMER;
         /* LEFT JOIN prxgt_dwnl_customer pdc */
         $tblDwnlCust = [
             $asDwnlCust => $this->resource->getTableName(\Praxigento\Downline\Data\Entity\Customer::ENTITY_NAME)
@@ -36,8 +40,8 @@ class Builder
         $on = $asDwnlCust . '.' . \Praxigento\Downline\Data\Entity\Customer::ATTR_CUSTOMER_ID . '='
             . $asDwnlSnap . '.' . \Praxigento\Downline\Data\Entity\Snap::ATTR_CUSTOMER_ID;
         $cols = [
-            self::AS_ATTR_MLM_ID => \Praxigento\Downline\Data\Entity\Customer::ATTR_HUMAN_REF,
-            self::AS_ATTR_COUNTRY_CODE => \Praxigento\Downline\Data\Entity\Customer::ATTR_COUNTRY_CODE
+            self::A_MLM_ID => \Praxigento\Downline\Data\Entity\Customer::ATTR_HUMAN_REF,
+            self::A_COUNTRY_CODE => \Praxigento\Downline\Data\Entity\Customer::ATTR_COUNTRY_CODE
         ];
         $result->joinLeft($tblDwnlCust, $on, $cols);
         /* LEFT JOIN customer_entity ce */
@@ -47,9 +51,10 @@ class Builder
         $on = $asCust . '.' . Cfg::E_CUSTOMER_A_ENTITY_ID . '='
             . $asDwnlCust . '.' . \Praxigento\Downline\Data\Entity\Customer::ATTR_CUSTOMER_ID;
         $cols = [
-            self::AS_ATTR_EMAIL => Cfg::E_CUSTOMER_A_EMAIL,
-            self::AS_ATTR_NAME_FIRST => Cfg::E_CUSTOMER_A_FIRSTNAME,
-            self::AS_ATTR_NAME_LAST => Cfg::E_CUSTOMER_A_LASTNAME
+            self::A_EMAIL => Cfg::E_CUSTOMER_A_EMAIL,
+            self::A_NAME_FIRST => Cfg::E_CUSTOMER_A_FIRSTNAME,
+            self::A_NAME_MIDDLE => Cfg::E_CUSTOMER_A_MIDDLENAME,
+            self::A_NAME_LAST => Cfg::E_CUSTOMER_A_LASTNAME
         ];
         $result->joinLeft($tblCust, $on, $cols);
         return $result;
