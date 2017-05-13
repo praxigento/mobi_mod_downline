adf655c41f73c6fb460a0f4264c330eb0c8ef6a1
Alex Gusev <alex@flancer64.com>
2017-05-13 18:03:49 +0300

    MOBI-715 - Add API operation to get data for Downline reports

--- src/Api/Tree/Get.php
+++ src/Api/Tree/Get.php
@@ -66,7 +66,7 @@ class Get
 
         /* only currently logged in  customer can get account statement */
         $currCustData = $this->authenticator->getCurrentCustomerData();
-        $currCustId = $currCustData->get(Cfg::E_CUSTOMER_A_ENTITY_ID);
+        $currCustId = $this->authenticator->getCurrentCustomerId();
         /** @var \Praxigento\Downline\Data\Entity\Customer $currDwnlData */
         $currDwnlData = $currCustData->get(\Praxigento\Downline\Infra\Api\Authenticator::A_DWNL_DATA);
         $currCustPath = $currDwnlData->getPath() . $currDwnlData->getCustomerId() . Cfg::DTPS;
