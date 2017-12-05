78abecf85f88ba2ae51546b2ccd1fe916dcfd789
Alex Gusev <alex@flancer64.com>
2017-12-05 14:15:54 +0200

    MOBI-1021 Refactor \Praxigento\Downline\Helper\Tree

--- src/Helper/Tree.php
+++ src/Helper/Tree.php
@@ -41,6 +41,71 @@ class Tree
     }
 
     /**
+     * Create map of the front team members (siblings) [$custId => [$memberId, ...], ...] from compressed or snapshot
+     * data.
+     *
+     * @param array|\Praxigento\Core\Data[] $data nested array or array of data objects.
+     * @param string $keyCustId name of the 'customer id' attribute.
+     * @param string $keyParentId name of the 'parent id' attribute.
+     *
+     * @return array [$custId => [$memberId, ...], ...]
+     */
+    public function mapByTeams($data, $keyCustId, $keyParentId)
+    {
+        $result = [];
+        foreach ($data as $one) {
+            if (is_array($one)) {
+                $customerId = $one[$keyCustId];
+                $parentId = $one[$keyParentId];
+            } else {
+                /* this should be data object */
+                $customerId = $one->get($keyCustId);
+                $parentId = $one->get($keyParentId);
+            }
+            if ($customerId == $parentId) {
+                /* skip root nodes, root node is not a member of a team. */
+                continue;
+            }
+            if (!isset($result[$parentId])) {
+                $result[$parentId] = [];
+            }
+            $result[$parentId][] = $customerId;
+        }
+        return $result;
+    }
+
+    /**
+     * Get depth index for Downline Tree ordered by depth desc.
+     *
+     * @param array|\Praxigento\Core\Data[] $tree nested array or array of data objects.
+     * @param string $keyCustId name of the 'customer id' attribute.
+     * @param string $keyDepth name of the 'depth' attribute.
+     *
+     * @return array  [$depth => [$custId, ...]]
+     */
+    public function mapByTreeDepthDesc($tree, $keyCustId, $keyDepth)
+    {
+        $result = [];
+        foreach ($tree as $one) {
+            if (is_array($one)) {
+                $customerId = $one[$keyCustId];
+                $depth = $one[$keyDepth];
+            } else {
+                /* this should be data object */
+                $customerId = $one->get($keyCustId);
+                $depth = $one->get($keyDepth);
+            }
+            if (!isset($result[$depth])) {
+                $result[$depth] = [];
+            }
+            $result[$depth][] = $customerId;
+        }
+        /* sort by depth desc */
+        krsort($result);
+        return $result;
+    }
+
+    /**
      * Map IDs by groups for the mapping key:
      *  [[id=>123, path=>':1:', ...], [id=>321, path=>':1:', ...], ]
      *  [':1:'=> [123, 321, ...], ...]
@@ -96,4 +161,5 @@ class Tree
         }
         return $result;
     }
+
 }
