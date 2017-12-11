a6e8fdb57ea23a2fa9cd628307bb1bd41db69aa0
Alex Gusev <alex@flancer64.com>
2017-12-11 19:52:01 +0200

    MOBI-1030 Auto registration for new customers

---
+++ src/Observer/SalesModelServiceQuoteSubmitSuccess.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Praxigento\Downline\Observer;
+
+
+use Magento\Framework\Event\Observer as Observer;
+
+class SalesModelServiceQuoteSubmitSuccess
+    implements \Magento\Framework\Event\ObserverInterface
+{
+    /** @var \Magento\Sales\Api\OrderCustomerManagementInterface */
+    private $manOrderCust;
+
+    public function __construct(
+        \Magento\Sales\Api\OrderCustomerManagementInterface $manOrderCust
+    ) {
+        $this->manOrderCust = $manOrderCust;
+    }
+
+    public function execute(Observer $observer) {
+        /** @var \Magento\Sales\Model\Order $order */
+        $order = $observer->getData('order');
+        /** @var \Magento\Quote\Model\Quote $quote */
+        $quote = $observer->getData('quote');
+
+        $orderId = $order->getId();
+        $this->manOrderCust->create($orderId);
+    }
+}
