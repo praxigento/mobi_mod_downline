8d211d16407b8d7621904ebcb62dd4b3ebdaaccb
Alex Gusev <alex@flancer64.com>
2017-12-31 21:59:19 +0200

    MOBI-1073 Clean up InstallScheme classes in modules

--- src/Setup/InstallSchema.php
+++ src/Setup/InstallSchema.php
@@ -3,6 +3,7 @@
  * Create DB schema.
  * User: Alex Gusev <alex@flancer64.com>
  */
+
 namespace Praxigento\Downline\Setup;
 
 use Praxigento\Downline\Repo\Entity\Data\Change;
@@ -11,27 +12,24 @@ use Praxigento\Downline\Repo\Entity\Data\Snap;
 
 class InstallSchema extends \Praxigento\Core\App\Setup\Schema\Base
 {
-    protected function _setup()
+    protected function setup()
     {
         /** Read and parse JSON schema. */
         $pathToFile = __DIR__ . '/../etc/dem.json';
         $pathToNode = '/dBEAR/package/Praxigento/package/Downline';
-        $demPackage = $this->_toolDem->readDemPackage($pathToFile, $pathToNode);
+        $demPackage = $this->toolDem->readDemPackage($pathToFile, $pathToNode);
 
         /* Customer */
-        $entityAlias = Customer::ENTITY_NAME;
         $demEntity = $demPackage->get('entity/Customer');
-        $this->_toolDem->createEntity($entityAlias, $demEntity);
+        $this->toolDem->createEntity(Customer::ENTITY_NAME, $demEntity);
 
         /* Change */
-        $entityAlias = Change::ENTITY_NAME;
         $demEntity = $demPackage->get('entity/Change');
-        $this->_toolDem->createEntity($entityAlias, $demEntity);
+        $this->toolDem->createEntity(Change::ENTITY_NAME, $demEntity);
 
         /* Snapshot */
-        $entityAlias = Snap::ENTITY_NAME;
         $demEntity = $demPackage->get('entity/Snapshot');
-        $this->_toolDem->createEntity($entityAlias, $demEntity);
+        $this->toolDem->createEntity(Snap::ENTITY_NAME, $demEntity);
     }
 
 
