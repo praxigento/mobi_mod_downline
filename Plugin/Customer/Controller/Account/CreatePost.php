0e2d8efb864ca52bf867f1d0a13fabe16e0942ee
Alex Gusev <alex@flancer64.com>
2018-05-22 18:36:22 +0300

    MOBI-1319 Disable sign in function

---
+++ Plugin/Customer/Controller/Account/CreatePost.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Authors: Alex Gusev <alex@flancer64.com>
+ * Since: 2018
+ */
+
+namespace Praxigento\Downline\Plugin\Customer\Controller\Account;
+
+use Magento\Framework\Controller\ResultFactory as AResultFactory;
+
+class CreatePost
+{
+    /**
+     * @var \Magento\Framework\Controller\ResultFactory
+     */
+    private $resultFactory;
+
+    public function __construct(
+        \Magento\Framework\Controller\ResultFactory $resultFactory
+    ) {
+        $this->resultFactory = $resultFactory;
+    }
+
+    /**
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function aroundExecute()
+    {
+//        $result = $proceed();
+        /** @var \Magento\Framework\Controller\Result\Raw $result */
+        $result = $this->resultFactory->create(AResultFactory::TYPE_RAW);
+        $result->setHttpResponseCode(\Magento\Framework\Webapi\Exception::HTTP_UNAUTHORIZED);
+        $result->setContents('Please, use referral link to register as customer.');
+        return $result;
+    }
+}
