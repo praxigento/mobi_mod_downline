803156383ddc1b9979a4a6640f5c319f81b702ac
Alex Gusev <alex@flancer64.com>
2018-10-01 17:21:36 +0300

    SAN-368 Customer creation failure from admin

--- Plugin/Magento/Customer/Model/AccountManagement.php
+++ Plugin/Magento/Customer/Model/AccountManagement.php
@@ -12,13 +12,17 @@ class AccountManagement
     private $daoCust;
     /** @var \Praxigento\Downline\Repo\Dao\Customer */
     private $daoDwnlCust;
+    /** @var \Praxigento\Downline\Helper\Registry */
+    private $hlpReg;
 
     public function __construct(
         \Magento\Customer\Api\CustomerRepositoryInterface $daoCust,
-        \Praxigento\Downline\Repo\Dao\Customer $daoDwnlCust
+        \Praxigento\Downline\Repo\Dao\Customer $daoDwnlCust,
+        \Praxigento\Downline\Helper\Registry $hlpReg
     ) {
         $this->daoCust = $daoCust;
         $this->daoDwnlCust = $daoDwnlCust;
+        $this->hlpReg = $hlpReg;
     }
 
     /**
@@ -48,5 +52,32 @@ class AccountManagement
             /* stealth exceptions */
         }
         return [$username, $password];
+    }
+
+    /**
+     * Extract country code and save into Magento registry when customer is created through adminhtml.
+     *
+     * @param \Magento\Customer\Model\AccountManagement $subject
+     * @param \Magento\Customer\Api\Data\CustomerInterface $customer
+     * @param null $password
+     * @param string $redirectUrl
+     * @return array
+     */
+    public function beforeCreateAccount(
+        \Magento\Customer\Model\AccountManagement $subject,
+        \Magento\Customer\Api\Data\CustomerInterface $customer,
+        $password = null,
+        $redirectUrl = ''
+    ) {
+        $addrs = $customer->getAddresses();
+        if (is_array($addrs)) {
+            foreach ($addrs as $addr) {
+                $countryCode = $addr->getCountryId();
+                $this->hlpReg->putCustomerCountry($countryCode);
+                $isBilling = $addr->isDefaultBilling();
+                if ($isBilling) break; // get any address but exit the loop if default billing
+            }
+        }
+        return [$customer, $password, $redirectUrl];
     }
 }
