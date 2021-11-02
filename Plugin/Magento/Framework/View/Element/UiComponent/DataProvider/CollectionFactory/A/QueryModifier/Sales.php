d753f95a09f00a47644782b6d929647af5246564
Alex Gusev <alex@flancer64.com>
2021-11-02 12:23:21 +0200

    SAN-1001 Add Filters to Orders List (Santegra magento)

--- Plugin/Magento/Framework/View/Element/UiComponent/DataProvider/CollectionFactory/A/QueryModifier/Sales.php
+++ Plugin/Magento/Framework/View/Element/UiComponent/DataProvider/CollectionFactory/A/QueryModifier/Sales.php
@@ -5,6 +5,7 @@
 
 namespace Praxigento\Downline\Plugin\Magento\Framework\View\Element\UiComponent\DataProvider\CollectionFactory\A\QueryModifier;
 
+use Praxigento\Core\App\Repo\Query\Expression as AnExpression;
 use Praxigento\Downline\Config as Cfg;
 use Praxigento\Downline\Repo\Data\Customer as EDwnlCust;
 
@@ -13,14 +14,17 @@ class Sales
     /** Tables aliases for external usage ('camelCase' naming) */
     private const AS_CUST = 'prxgtDwnlCust';
     private const AS_PARENT = 'prxgtDwnlParent';
+    private const AS_MAGE_PARENT = 'mageCustParent';
 
     /** Columns/expressions aliases for external usage ('camelCase' naming) */
     private const A_COUNTRY = 'prxgtDwnlCountry';
     private const A_MLM_ID = 'prxgtDwnlMlmId';
     private const A_PARENT_MLM_ID = 'prxgtDwnlMlmIdParent';
+    private const A_PARENT_NAME = 'prxgtDwnlParentName';
 
     /** Entities are used in the query */
     private const E_CUST = EDwnlCust::ENTITY_NAME;
+    private const E_MAGE_PARENT = Cfg::ENTITY_MAGE_CUSTOMER;
     private const E_PARENT = EDwnlCust::ENTITY_NAME;
 
     /** @var \Magento\Framework\App\ResourceConnection */
@@ -47,6 +51,10 @@ class Sales
         $fieldAlias = self::A_PARENT_MLM_ID;
         $fieldFullName = self::AS_PARENT . '.' . EDwnlCust::A_MLM_ID;
         $collection->addFilterToMap($fieldAlias, $fieldFullName);
+        /* prxgtDwnlParentName */
+        $fieldAlias = self::A_PARENT_NAME;
+        $fieldFullName = $this->expCustName();
+        $collection->addFilterToMap($fieldAlias, $fieldFullName);
     }
 
     public function populateSelect(
@@ -55,6 +63,7 @@ class Sales
         $result = $collection->getSelect();
         $asCust = self::AS_CUST;
         $asParent = self::AS_PARENT;
+        $asParentName = self::AS_MAGE_PARENT;
 
         /* LEFT JOIN prxgt_dwnl_customer (for customer MLM ID) */
         $tbl = $this->resource->getTableName(self::E_CUST);
@@ -75,6 +84,22 @@ class Sales
         $cond = "$as." . EDwnlCust::A_CUSTOMER_REF . "=$asCust." . EDwnlCust::A_PARENT_REF;
         $result->joinLeft([$as => $tbl], $cond, $cols);
 
+        /* LEFT JOIN customer_entity (for parent name) */
+        $tbl = $this->resource->getTableName(self::E_MAGE_PARENT);
+        $as = $asParentName;
+        $cols = [
+            self::A_PARENT_NAME => $this->expCustName()
+        ];
+        $cond = "$as." . Cfg::E_CUSTOMER_A_ENTITY_ID . "=$asCust." . EDwnlCust::A_PARENT_REF;
+        $result->joinLeft([$as => $tbl], $cond, $cols);
+
+        return $result;
+    }
+
+    private function expCustName() {
+        $name = 'CONCAT(' . self::AS_MAGE_PARENT . '.' . Cfg::E_CUSTOMER_A_FIRSTNAME . ', " ", '
+            . self::AS_MAGE_PARENT . '.' . Cfg::E_CUSTOMER_A_LASTNAME . ')';
+        $result = new AnExpression($name);
         return $result;
     }
 
